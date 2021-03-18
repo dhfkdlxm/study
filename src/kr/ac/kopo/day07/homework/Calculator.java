e0773685fef209367474984741ee6ac1b9c2c03a
DESKTOP-BRT4UDV\Hwany <dhfkdlxm@naver.com>
2021-03-18 21:10:26 +0900

    //

--- src/kr/ac/kopo/day07/homework/Calculator.java
+++ src/kr/ac/kopo/day07/homework/Calculator.java
@@ -4,27 +4,52 @@ import java.util.Scanner;
 
 public class Calculator {
 	Scanner sc = new Scanner(System.in);
-
+	
+	/**
+	 *  더하기
+	 * @param a 입력 정수
+	 * @param b 입력 정수
+	 * @return 결과 출력
+	 */
 	int add(int a, int b) {
-		
+
 		return a + b;
 	}
-
+	/**
+	 *  빼기
+	 * @param a 입력 정수
+	 * @param b 입력 정수
+	 * @return 결과 출력
+	 */
 	int minus(int a, int b) {
 		
 		return a - b;
 	}
-
+	/**
+	 *  곱하기
+	 * @param a 입력 정수
+	 * @param b 입력 정수
+	 * @return 결과 출력
+	 */
 	int multi(int a, int b) {
 
 		return a * b;
 	}
-
+	/**
+	 *  나누기
+	 * @param a 입력 정수
+	 * @param b 입력 정수
+	 * @return 결과 출력
+	 */
 	double div(double a, double b) {
 
 		return a / b;
 	}
-
+/**
+ * 소수 구하기
+ * @param a 입력 정수
+ * @return 소수 여부 반환(Boolean)
+ */
 	boolean prime(int a) {
 		boolean isprime = true;
 		for (int i = 2; i < a / 2; i++) {
@@ -35,6 +60,11 @@ public class Calculator {
 		return isprime;
 	}
 
+	/**
+	 * 
+	 * @param string 입력 멘트
+	 * @return 입력값 반환
+	 */
 	int getNum(String string) {
 		System.out.print(string);
 		int num = sc.nextInt();
@@ -42,11 +72,26 @@ public class Calculator {
 		return num;
 	}
 	
-	void print(int a, int b, char c, double d) {
-		System.out.println(a+ ""+c+ b+ "="+d);
+	/**
+	 * 사칙연산 결과 출력
+	 * @param a 입력 정수
+	 * @param b 입력 정수
+	 */
+	
+	void printCal(int a, int b) {
+		System.out.println(a+" + "+b+" = "+add(a,b));
+		System.out.println(a+" - "+b+" = "+minus(a,b));
+		System.out.println(a+" * "+b+" = "+multi(a,b));
+		System.out.println(a+" / "+b+" = "+div(a,b));
 	}
-	void print(int a, boolean bool) {
-		System.out.println(a+" 소수체크 : " + bool );
+	
+	/**
+	 * 소수 판단 결과 출력
+	 * @param a 입력 정수
+	 */
+	
+	void prime_print(int a) {
+		System.out.println(a+" 소수체크 : " + prime(a));
 	}
 	
 	
--- src/kr/ac/kopo/day07/homework/IceCream.java
+++ src/kr/ac/kopo/day07/homework/IceCream.java
@@ -1,17 +1,83 @@
 package kr.ac.kopo.day07.homework;
 
+import java.util.Scanner;
+
 public class IceCream {
+	Scanner sc = new Scanner(System.in);
 	String name;
 	int price;
 	
-	IceCream[] Ice(int num){
+	/**
+	 * 아이스크림 구매정보 입력 메소드
+	 * 
+	 * @param num 아이스크림 개수 입력
+	 * @return 아이스크림 구매정도 배열 반환
+	 */
+	
+	IceCream[] iceInfo(int num) {
 		IceCream[] ic = new IceCream[num];
+		for (int i = 0; i < ic.length; i++) {
+			ic[i] = new IceCream();
+			System.out.println("***"+(i+1)+"번째 아이스크림 구매 정보 입력");
+			ic[i].name = getStr("아이스크림명 : ");  
+			ic[i].price = getNum("아이스크림 가격 : ");
+		}
 		return ic;
 	}
+	
+	/**
+	 * 아이스크림 가격 입력 메소드
+	 * @param IceCount 아이스크림 가격 입력 멘트
+	 * @return 아이스크림 가격 반환
+	 */
+	
+	int getNum(String IceCount) {
+		System.out.println(IceCount);
+		int num = sc.nextInt();
+		sc.nextLine(); 
+		return num;
+	}
 
+	/**
+	 * 아이스크림 이름 입력
+	 * @param IceCount 
+	 * @return 아이스크림 이름 출력
+	 */
+	
+	String getStr(String IceCount) {
+		System.out.println(IceCount);
+		String str = sc.nextLine();
+		return str;
+	}
 	
 	
+	/**
+	 * 아이스크림 배열을 받아 구매정보를 출력
+	 * @param ic 아이스크림 배열
+	 */
+	void printInfo(IceCream[] ic) {
+		System.out.println();
+		System.out.println("< 총 4개의 아이스크림 구매정보 출력 >");
+		System.out.println("번호\t"+"아이스크림\t"+"아이스크림 가격");
+		int idx = 1;
+		int result = 0;
+		for (IceCream i : ic) {
+			System.out.println( idx++ +"\t"+i.name+"\t\t"+i.price);
+		result += i.price;
+		}
+		iceCount(result);
+	}
 	
+	/**
+	 * 아이스크림 총합 출력 받는 메소드
+	 * @param result 가격 총합 출력
+	 */
+	void iceCount(int result) {
+		System.out.println("총 계 : "+ result + "원");
+	}
 	
+	void iceCount(Double result) {
+		
+	}
 	
 }
